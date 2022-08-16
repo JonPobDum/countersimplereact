f037ead61ce9bed0565cfb37dde77803e6b1a73e
JonPobDum <jpoblete86@gmail.com>
2022-08-16 23:58:45 +0000

    ejercicio inconcluso

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,27 @@
 //import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
+import PropTypes from 'prop-types';
 
 // include your styles into the webpack bundle
 import "../styles/index.css";
 
 //import your own components
-import Home from "./component/home.jsx";
+import Counter from "./component/home.jsx";
 
-//render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+let ticTac =0;
+
+setInterval(function(){
+    const centenaMil = Math.floor(ticTac/100000)
+    const decenaMil = Math.floor(ticTac/10000)
+    const unidadMil = Math.floor(ticTac/1000)
+    const centena = Math.floor(ticTac/100)
+    const decena = Math.floor(ticTac/10)
+    const unidad = Math.floor(ticTac/1)
+    console.log(centenaMil, decenaMil, unidadMil, centena, decena, unidad);
+    counter++;
+
+   
+
+ReactDOM.render(<Counter digitUnidad={ticTac.indexOf()}/>, document.querySelector("#app"));
+
